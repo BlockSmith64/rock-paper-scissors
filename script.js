429cbb8472862d454004e420830a2a72f92ff86a
Kabilla <a.hadooke3141@gmail.com>
2022-05-23 18:45:51 +0300

    added a new game button

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ const computerPlay = function () {
 let botScore = 0;
 let humanScore = 0;
 
+let gameOn = true;
+const game = function () {
+  // for (let i = 0; i < 5; i++) {
+  //   console.log(playRound());
+  //   console.log(`Score: huamans (${humanScore}:${botScore}) bots`);
+  // }
+
+  if (humanScore === 5) {
+    result.textContent = "Humans Win the game!";
+    return (gameOn = false);
+  } else if (botScore === 5) {
+    result.textContent = "Bots Win the game!";
+    return (gameOn = false);
+  }
+};
+
 const playRound = function (playerSelection, computerSelection) {
   computerSelection = computerPlay();
   playerSelection = "rock";
@@ -13,22 +29,22 @@ const playRound = function (playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return `It's a Tie! you both chose ${playerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    botScore++;
+    ++botScore;
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    humanScore++;
+    ++humanScore;
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    botScore++;
+    ++botScore;
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    humanScore++;
+    ++humanScore;
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    botScore++;
+    ++botScore;
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "scisors" && computerSelection === "paper") {
-    humanScore++;
+    ++humanScore;
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
 };
@@ -38,96 +54,91 @@ const btnPaper = document.querySelector(".btn-paper");
 const btnScissors = document.querySelector(".btn-scissors");
 
 const score = document.querySelector(".score");
-
+const result = document.querySelector(".result");
 score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
 
 btnRock.addEventListener("click", function () {
-  computerSelection = computerPlay();
-  playerSelection = "rock";
+  game();
+  if (gameOn) {
+    computerSelection = computerPlay();
+    playerSelection = "rock";
 
-  if (playerSelection === computerSelection) {
-    console.log("draw");
-    score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
-    return `It's a Tie! you both chose ${playerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    botScore++;
-    console.log("L");
-    score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
-    return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log("W");
-    humanScore++;
-    `Score: huamans (${humanScore}:${botScore}) bots`;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    if (playerSelection === computerSelection) {
+      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+      return `It's a Tie! you both chose ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+      ++botScore;
+      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      ++humanScore;
+      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
   }
 });
 
 btnPaper.addEventListener(
   "click",
   function (playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = "paper";
+    game();
+    if (gameOn) {
+      computerSelection = computerPlay();
+      playerSelection = "paper";
 
-    if (playerSelection === computerSelection) {
-      console.log("draw");
-      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
-      return `It's a Tie! you both chose ${playerSelection}`;
-    } else if (
-      playerSelection === "paper" &&
-      computerSelection === "scissors"
-    ) {
-      botScore++;
-      console.log("L");
-      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      humanScore++;
-      console.log("W");
-      score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
+      if (playerSelection === computerSelection) {
+        score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+        return `It's a Tie! you both chose ${playerSelection}`;
+      } else if (
+        playerSelection === "paper" &&
+        computerSelection === "scissors"
+      ) {
+        ++botScore;
+        score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        ++humanScore;
+        score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+      }
     }
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
 );
 
-btnScissors.addEventListener(
-  "click",
-  function (playerSelection, computerSelection) {
+btnScissors.addEventListener("click", function () {
+  game();
+  if (gameOn) {
     computerSelection = computerPlay();
     playerSelection = "scissors";
 
     if (playerSelection === computerSelection) {
-      console.log("draw");
       score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
       return `It's a Tie! you both chose ${playerSelection}`;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      botScore++;
-      console.log("L");
+      ++botScore;
       score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
       return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else if (
       playerSelection === "scissors" &&
       computerSelection === "paper"
     ) {
-      humanScore++;
-      console.log("W");
+      ++humanScore;
       score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
       return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
-);
+});
 
-const game = function () {
-  // for (let i = 0; i < 5; i++) {
-  //   console.log(playRound());
-  //   console.log(`Score: huamans (${humanScore}:${botScore}) bots`);
-  // }
-  if (humanScore > 5) {
-    console.log("Humans Win the game!");
-  } else if (botScore > 5) {
-    console.log("Bots Win the game!");
-  }
-};
+const body = document.querySelector("body");
+const btnNewGame = document.createElement("button");
+btnNewGame.textContent = "New Game";
 
-console.log(game());
+body.appendChild(btnNewGame);
+
+btnNewGame.addEventListener("click", function () {
+  gameOn = true;
+  botScore = 0;
+  humanScore = 0;
+  score.textContent = `Score: huamans (${humanScore}:${botScore}) bots`;
+  result.textContent = "";
+});
